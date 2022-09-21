28ec25a211cba5d1dc891baaa082ec864d0eb523
tranminhhuy <tranminhhuy30042001@gmail.com>
2022-09-21 05:46:24 -0400

    Fix 9 is not exists in range and Add CONST to easy update
    (cherry picked from commit d9c0ae50e3e29be9aa286336adca229f8387b975)

--- task3/array_basic_2.rb
+++ task3/array_basic_2.rb
@@ -1,14 +1,17 @@
+MAX_INPUT = 99
+MIN_INPUT = 0
+RANGE_RANDOM = 1..9
 
 begin #check input n
-    print 'Nhap n(n <= 99): '
+    print "Nhap n(n <= #{MAX_INPUT}): "
     arr_size = Integer(gets.chomp,10)
-    raise if (arr_size > 99 || arr_size < 0)
+    raise if (arr_size > MAX_INPUT || arr_size < MIN_INPUT)
 rescue
-    puts 'n phai <= 99'
+    puts "n phai <= #{MAX_INPUT}"
     retry
 end
   
-arr = Array.new(arr_size) { rand(1...9) }
+arr = Array.new(arr_size) { rand(RANGE_RANDOM) }
 
 p arr
 
