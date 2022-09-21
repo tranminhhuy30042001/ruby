1b51c33e3399d4188b6023b3fe09795ece337f35
tranminhhuy <tranminhhuy30042001@gmail.com>
2022-09-21 03:47:46 -0400

    Update change negative number not is input
    (cherry picked from commit e16195f827d18ae6d836ce3b6426389b220beb07)

--- task1/array_basic.rb
+++ task1/array_basic.rb
@@ -3,13 +3,16 @@ EXIT_INPUT = -1
 arr = Array.new
 
 def get_integer_num(label)
+    output = 1
     begin
         print "#{label}"
-        Integer(gets.chomp)
+        output = Integer(gets.chomp)
+        raise if (output < -1) 
     rescue
-       puts 'vui long nhap kieu du lieu interger'
+       puts 'vui long nhap kieu du lieu so nguyen'
        retry
     end
+    return output
 end
 
 puts 'nhap -1 de thoat'
