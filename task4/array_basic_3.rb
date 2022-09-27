98251590bd0ea2a1b8738b89136c465975734e2c
tranminhhuy <tranminhhuy30042001@gmail.com>
2022-09-27 00:53:39 -0400

    Make code beautiful

--- task4/array_basic_3.rb
+++ task4/array_basic_3.rb
@@ -1,14 +1,15 @@
-numbers = [3, 5, -4, 8, 11, 1, 7, 6]
+numbers = [3, 5, -4, 8, 11, 1, -1, 6]
 target_sum = 10
 
 
 def task4(numbers, target_sum)
     hsh = Hash.new
     numbers.each do |value|
+        other_value = target_sum - value
         if(hsh[value])
-            return [target_sum - value,value]
+            return [other_value,value]
         else
-            hsh[target_sum - value] = 1
+            hsh[other_value] = 1
         end
     end
     []
